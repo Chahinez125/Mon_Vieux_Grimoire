0e328a3c1827f582cc0be38870060cc9d68b12a6
Chahinez125 <chahinez.semmar@yahoo.com>
2024-06-20 21:26:13 +0200

    Connexion à MongoDB réussie ajout fichier .env

--- Backend/app.js
+++ Backend/app.js
@@ -1,16 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+require('dotenv').config(); // Charger les variables d'environnement à partir du fichier .env
+// Utiliser la variable d'environnement pour l'URL de MongoDB
+const mongoDBUri = process.env.MONGODB_URI;
+
+mongoose.connect(mongoDBUri)
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch((err) => console.log('Connexion à MongoDB échouée !', err));
 
 const app = express();
-app.use(express.json());
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
-app.post((req, res, next) => {
 
-  next();
+app.use(bodyParser.json());
+
+// Définir une route de base
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Exemple de route POST
+app.post('/test', (req, res) => {
+  res.status(200).send('Post request received');
 });
 
 module.exports = app;
