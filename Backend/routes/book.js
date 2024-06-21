22c44a77b64d0795b7c077434299c71593b14c50
Chahinez125 <chahinez.semmar@yahoo.com>
2024-06-21 02:16:51 +0200

    creation fichier controllers pour organiser backend pour simplifier la lecteur

--- Backend/routes/book.js
+++ Backend/routes/book.js
@@ -1,41 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const Book = require('../models/book');
+const bookCtrl = require('../controllers/book');
 
 // Exemple de route POST
-router.post('/',async (req, res,next) => {
-    delete req.body._id;
-    const book = new Book({
-        ...req.body
-        });
-    book.find()
-    .then((books) => res.status(200).json(books))
-    
-    .catch(error => {
-      res.status(400).json({ error });
-  });
-  });
-  router.put('/:id', (req, res, next) => {
-    Book.updateOne({ _id: req.params.id },{...req.body, _id: req.params.id})
-      .then(() => res.status(200).json({ message: 'Objet modifié!'}))
-      .catch(error => res.status(400).json({ error }));
-  });
- router.delete('/:id', (req, res, next) => {
-    Book.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé!'}))
-      .catch(error => res.status(404).json({ error }));
-  });
-  // Route pour récupérer un livre par son ID
-  router.get('/:id', (req, res, next) => {
-    Book.findOne({ _id: req.params.id })
-      .then(book => res.status(200).json(book))
-      .catch(error => res.status(404).json({ error }));
-  });
+router.post('/', bookCtrl.createBook);
+  router.put('/:id', bookCtrl.modifyBook);
+ router.delete('/:id', bookCtrl.deletBook);
+  
+  router.get('/:id', bookCtrl.getOneBook);
 
-  router.get('/', (req, res, next) => {
-    Book.find()
-      .then(books => res.status(200).json(books))
-      .catch(error => res.status(400).json({ error }));
-  });
+  router.get('/', bookCtrl.getAllBooks);
 
   module.exports = router;
